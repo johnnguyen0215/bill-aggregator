9ce560a4771f8cd77ad3b7e3d63b23ecc8de070d
John Nguyen <john.nguyen@mheducation.com>
2021-01-03 02:31:15 -0800

    sve work

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import './App.css';
-import { useEffect, useCallback, useState } from 'react';
+import { useEffect, useCallback, useState, useRef } from 'react';
 
-import { Button, Container, Backdrop, CircularProgress, AppBar, Toolbar } from '@material-ui/core';
+import { Button, Container, Backdrop, CircularProgress, AppBar, Toolbar, TextField } from '@material-ui/core';
 
 const gapi = window.gapi;
 
 function App() {
-  const [isSignedIn, setIsSignedIn] = useState(false);
+  const [isSignedIn, setIsSignedIn] = useState(null);
   const [pending, setPending] = useState(false);
+  const [totalBill, setTotalBill] = useState(0);
+  const [manualInputValue, setManualInputValue] = useState();
+
+  const billsInfo = useRef([
+    {
+      email: 'customerservice@socalgas.com',
+      subject: 'Your Bill from Southern California Gas Company'
+    },
+    {
+      email: 'ibp3@scewebservices.com',
+      subject: 'SCE Bill is Ready to View'
+    },
+    {
+      email: 'geico@email1.geico.com',
+      subject: 'consider reviewing this pending GEICO payment'
+    }
+  ])
 
   const initGapi = useCallback(async () => {
     await gapi.client.init({
@@ -18,8 +35,6 @@ function App() {
     });
 
     gapi.auth2.getAuthInstance().isSignedIn.listen((isSignedIn) => {
-      console.log('Setting signed in value');
-      console.log(isSignedIn);
       setIsSignedIn(isSignedIn);
       setPending(false);
     });
@@ -27,22 +42,71 @@ function App() {
     setIsSignedIn(gapi.auth2.getAuthInstance().isSignedIn.get());
   }, []);
 
-  const handleGetMessages = async () => {
-    const response = await gapi.client.gmail.users.messages.list({ userId: "me", maxResults: 10, q: "from:customerservice@socalgas.com"});
-    const responseObject = JSON.parse(response.body);
+  const isDateInRange = (dateStr) => {
+    const today = new Date();
 
-    const messageId = responseObject.messages[0].id;
-    const messageResponse = await gapi.client.gmail.users.messages.get({ userId: "me", id: messageId });
+    const lastMonth = new Date();
+    lastMonth.setDate(1);
+    lastMonth.setMonth(lastMonth.getMonth()-1);
 
-    const messageResponseBody = JSON.parse(messageResponse.body);
+    //"Fri, 04 Dec 2020 06:34:42 +0000 (UTC)";
+    const date = new Date(dateStr);
 
-    const partData = messageResponseBody.payload.parts[0].body.data;
+    return date <= today && date >= lastMonth;
+  }
 
-    const partBody = atob( partData.replace(/-/g, '+').replace(/_/g, '/') );
+  const queryBuilder = (email, subject, afterDate, beforeDate) => {
+    return `from:${email} AND subject:${subject}`;
+  }
 
-    const billAmount = parseFloat(partBody.match(/\$\s*\d*.\d{2}/)[0].slice(1));
+  // Algorithm, use the messages in between beginning of month to current date.
+  // If two results are returned, take the later date.
+  // If one result is returned and the date occurs before the middle of the month
+  // then that is an old bill and should not be included in the final amount
 
-    console.log(billAmount);
+  const handleGetTotalBill= async () => {
+    setPending(true);
+
+    let totalBillAmount = 0;
+
+    for (let i = 0; i < billsInfo.current.length; i++) {
+      const billInfo = billsInfo.current[i];
+
+      const response = await gapi.client.gmail.users.messages.list(
+        {
+          userId: "me",
+          maxResults: 10,
+          q: queryBuilder(billInfo.email, billInfo.subject)
+        });
+
+      const responseObject = JSON.parse(response.body);
+
+      const messageId = responseObject.messages[0].id;
+      const messageResponse = await gapi.client.gmail.users.messages.get({ userId: "me", id: messageId });
+
+      const messageResponseBody = JSON.parse(messageResponse.body);
+
+      const dateHeader = messageResponseBody.payload.headers.find((header) => {
+        return header.name === 'Date';
+      });
+
+      const date = dateHeader.value;
+
+      if (isDateInRange(date)) {
+        const partData = messageResponseBody.payload.parts[0].body.data;
+
+        const partBody = atob(partData.replace(/-/g, '+').replace(/_/g, '/'));
+
+        const billAmount = parseFloat(partBody.match(/\$\s*\d*.\d{2}/)[0].slice(1));
+
+        setPending(false);
+
+        totalBillAmount += billAmount;
+      }
+    }
+
+    setPending(false);
+    setTotalBill(totalBillAmount);
   }
 
   const handleSignin = async () => {
@@ -53,6 +117,17 @@ function App() {
   const handleSignout = async () => {
     setPending(true);
     await gapi.auth2.getAuthInstance().signOut();
+  }
+
+  const handleInputOnChange = async (event) => {
+    setManualInputValue(event.target.value);
+  }
+
+  const handleInputKeydown = async (event) => {
+    console.log(event);
+    if (event.key === 'Enter') {
+      setTotalBill(totalBill + parseFloat(manualInputValue));
+    }
   }
 
   useEffect(() => {
@@ -75,32 +150,52 @@ function App() {
 
   return (
     <div className="App">
+      <Backdrop className="backdrop" open={pending}>
+        <CircularProgress color="secondary" />
+      </Backdrop>
       <AppBar position="static">
         <Toolbar className="toolbar">
           {
-            !isSignedIn ?
+            isSignedIn !== null && (!isSignedIn ?
             <div id="google-signin-button" onClick={handleSignin} /> :
-            <Button variant="contained" onClick={handleSignout} color="secondary">Sign Out</Button>
+            <Button variant="contained" onClick={handleSignout} color="secondary">Sign Out</Button>)
           }
         </Toolbar>
       </AppBar>
       <Container>
         <h1>Bill Aggregator</h1>
         <div className="homePage">
-
           {
             isSignedIn &&
             (
               <>
-                <Button variant="contained" onClick={handleGetMessages} color="secondary">Get Messages</Button>
+                <Button variant="contained" onClick={handleGetTotalBill} color="secondary">Get Total</Button>
               </>
             )
           }
         </div>
+        <div className="manualInput">
+          <TextField
+            onChange={handleInputOnChange}
+            onKeyDown={handleInputKeydown}
+            value={manualInputValue}
+            type="number"
+          />
+          <Button variant="contained" onClick={() => setTotalBill(manualInputValue + totalBill)} color="primary">Submit</Button>
+          <Button variant="contained" onClick={() => setTotalBill(0)} color="primary">Clear</Button>
+        </div>
+        {
+         !pending &&
+          <div className="billTotals">
+            <h2>Bill Total</h2>
+            {
+              totalBill ?
+                totalBill :
+                'No bills'
+            }
+          </div>
+        }
       </Container>
-      <Backdrop className="backdrop" open={pending}>
-        <CircularProgress color="secondary" />
-      </Backdrop>
     </div>
   );
 }
